c9f99804e7fa66170e633f4d514567108d98e291
Toasty23 <47454244+Toasty23@users.noreply.github.com>
2023-03-09 12:49:11 +0200

    Updated

---
+++ js/js.js
@@ -0,0 +1,13 @@
+var myname = "Joona Klaar";
+
+var course1 = "Javascript for Beginners.";
+var course2 = "Python for Beginners.";
+alert(course1 + course2 + myname + age);
+
+console.log(myname)
+
+var age = "30";
+
+console.warn("WARNING STRANGER DANGER")
+console.log("name:", myname, profession)
+console.error("ERROR ERROR ERROR ERROR")
